102980457703c97f94ca6d4af0a57ff03ac8d814
AlexStolbov <stolbov_alexey@mail.ru>
2020-12-05 22:39:30 +0300

    fix game_even.py

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -3,6 +3,12 @@ from prompt import string
 
 COUNT_Q = 3
 GAME_RULE = 'Answer "yes" if this number is even, otherwise answer "no"'
+MESSAGE_ONE = 'Question: {}'
+MESSAGE_TWO = 'Your answer: '
+MESSAGE_THREE = 'Correct!'
+MESSAGE_FOUR = '{} is wrong answer ;(. Correct answer was {}'
+MESSAGE_FIVE = 'Let\'s try again, {}'
+MESSAGE_SIX = 'Congratulations, {}'
 
 
 def get_question_and_answer():
@@ -17,17 +23,17 @@ def run_game(user_name, rule, get_question_and_correct_answer):
     wrong = False
     while correct_count < COUNT_Q and not wrong:
         question, correct_answer = get_question_and_correct_answer()
-        print('Question: {}'.format(question))
-        answer = string('Your answer: ')
+        print(MESSAGE_ONE.format(question))
+        answer = string(MESSAGE_TWO)
         if answer == correct_answer:
             correct_count += 1
-            print('Correct!')
+            print(MESSAGE_THREE)
         else:
-            print('{} is wrong answer ;(. Correct answer was {}'.format(answer, correct_answer))
-            print('Let\'s try again, {}'.format(user_name))
+            print(MESSAGE_FOUR.format(answer, correct_answer))
+            print(MESSAGE_FIVE.format(user_name))
             wrong = True
     if not wrong:
-        print('Congratulations, {}'.format(user_name))
+        print(MESSAGE_SIX.format(user_name))
 
 
 def run_game_even(user_name):
