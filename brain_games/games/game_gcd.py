009b40152ea30652f02c05173ed6dafe2f824c06
Alex <stolbov_alexey@mail.ru>
2020-12-09 22:39:02 +0300

    fix complexity(3) game_gcd

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -7,10 +7,6 @@ GAME_RULE = 'Find the greatest common divisor of given numbers.'
 def get_question_and_answer():
     number_one = randint(1, 20)
     number_two = randint(1, 20)
-
-    number_one = 25
-    number_two = 50
-
     question = '{} {}'.format(number_one, number_two)
     answer = gcd(number_one, number_two)
     return question, answer
@@ -18,7 +14,7 @@ def get_question_and_answer():
 
 def gcd(number_one, number_two):
     result = 1
-    while number_one > 1 and number_two > 1:
+    while number_one > 0 and number_two > 0:
         while number_one % 2 == 0 and number_two % 2 == 0:
             number_one = int(number_one / 2)
             number_two = int(number_two / 2)
@@ -27,14 +23,11 @@ def gcd(number_one, number_two):
             number_one = int(number_one / 2)
         while number_two % 2 == 0:
             number_two = int(number_two / 2)
-        if number_one == number_two:
-            result = str(result * number_one)
-            break
-        elif number_one > number_two:
-            number_one = int((number_one - number_two) / 2)
+        if number_one >= number_two:
+            number_one = number_one - number_two
         else:
-            number_two = int((number_two - number_one) / 2)
-    return str(result)
+            number_two = number_two - number_one
+    return str(result * number_two)
 
 
 def is_even(number):
