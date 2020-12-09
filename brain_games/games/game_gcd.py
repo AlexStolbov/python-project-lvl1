817b01826d007897315dc5cc9170b5bf3634de16
Alex <stolbov_alexey@mail.ru>
2020-12-09 22:07:48 +0300

    fix compexity in game_gcd.py

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -8,6 +8,9 @@ def get_question_and_answer():
     number_one = randint(0, 20)
     number_two = randint(1, 20)
 
+    number_one = 25
+    number_two = 50
+
     question = '{} {}'.format(number_one, number_two)
     answer = gcd(number_one, number_two)
     return question, answer
@@ -22,22 +25,21 @@ def gcd(number_one, number_two):
         return '1'
     result = 1
     while number_one > 1 and number_two > 1:
+        while number_one % 2 == 0 and number_two % 2 == 0:
+            number_one = int(number_one / 2)
+            number_two = int(number_two / 2)
+            result = result * 2
+        while number_one % 2 == 0:
+            number_one = int(number_one / 2)
+        while number_two % 2 == 0:
+            number_two = int(number_two / 2)
         if number_one == number_two:
             result = str(result * number_one)
             break
-        elif not is_even(number_one) and not is_even(number_two):
-            if number_one > number_two:
-                number_one = int((number_one - number_two) / 2)
-            else:
-                number_two = int((number_two - number_one) / 2)
-        elif is_even(number_one) and is_even(number_two):
-            result = result * 2
-            number_one = int(number_one / 2)
-            number_two = int(number_two / 2)
-        elif is_even(number_one):
-            number_one = int(number_one / 2)
+        elif number_one > number_two:
+            number_one = int((number_one - number_two) / 2)
         else:
-            number_two = int(number_two / 2)
+            number_two = int((number_two - number_one) / 2)
     return str(result)
 
 
