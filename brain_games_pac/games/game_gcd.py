db39ac7bff5c4bdb274d6d4276c105f8a36f0e1d
Alex <stolbov_alexey@mail.ru>
2020-12-19 10:29:44 +0300

    fix game_gcd

--- brain_games_pac/games/game_gcd.py
+++ brain_games_pac/games/game_gcd.py
@@ -9,7 +9,7 @@ def get_question_and_answer():
     number_one = randint(1, 20)
     number_two = randint(1, 20)
     question = '{} {}'.format(number_one, number_two)
-    answer = str(gcd)
+    answer = str(gcd(number_one, number_two))
     return question, answer
 
 
